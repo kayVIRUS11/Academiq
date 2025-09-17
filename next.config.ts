e27288f2da22a4aeb9df21c10b8a030ac2b363af
John Theodore <johntheodore0717@gmail.com>
2025-09-17 07:14:33 +0000

    The error happened because the service worker (sw.js) was not being serv

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
 require('dotenv').config({ path: './.env' });
 import type { NextConfig } from 'next';
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development",
+});
 
 const nextConfig: NextConfig = {
   typescript: {
@@ -40,4 +48,4 @@ const nextConfig: NextConfig = {
   }
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
