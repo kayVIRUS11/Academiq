16649a7d76c8d247a8333104015dab98538b6f23
John Theodore <johntheodore0717@gmail.com>
2025-09-18 10:18:23 +0000

    The image looks small. Is it because of the size? Do i crop it so that i

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -7,8 +7,8 @@ export function Logo({ className, isCollapsed }: { className?: string, isCollaps
       <Image 
         src="/icons/icon_file.png" 
         alt="Academiq Logo"
-        width={28}
-        height={28}
+        width={36}
+        height={36}
         className="shrink-0"
       />
       <span className={cn("text-xl font-bold tracking-tight transition-opacity", isCollapsed ? 'opacity-0 w-0' : 'opacity-100')}>Academiq</span>
